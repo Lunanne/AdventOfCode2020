c37fb3754924f56008b8c9c6d9bf48b1871cbc00
Anne Marije van der Meer <anne.marije.meer@gmail.com>
2021-09-13 16:44:05 +0200

    day 1 part 2

--- day1/src/day1.rs
+++ day1/src/day1.rs
@@ -14,13 +14,35 @@ pub(crate) fn part1(numbers: Vec<i32>) -> (i32, i32) {
     return (0, 0);
 }
 
+pub(crate) fn part2(numbers: Vec<i32>) -> (i32, i32, i32) {
+    let mut queue = VecDeque::from_iter(numbers.iter());
+    let secondQueue = queue.clone();
+    while !queue.is_empty() {
+        let x: i32 = *queue.pop_front().unwrap();
+        for &y in queue.iter() {
+            for &z in secondQueue.iter() {
+                if x + y + z == 2020 {
+                    return (x, *y, *z);
+                }
+            }
+        }
+    }
+    return (0, 0, 0);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_day1() {
+    fn test_part1() {
         let input = vec![1721, 979, 366, 299, 675, 1456];
         assert_eq!(part1(input), (1721, 299));
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = vec![1721, 979, 366, 299, 675, 1456];
+        assert_eq!(part2(input), (979, 366, 675));
     }
 }
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,6 @@
 extern crate utils;
 
-use crate::day1::part1;
+use crate::day1::{part1, part2};
 use crate::utils::read_lines;
 
 mod day1;
@@ -8,8 +8,10 @@ mod day1;
 fn main() {
     let input_text = include_str!("../input/day1_input.txt");
     let lines = read_lines(input_text);
-    let input = lines.iter().map(|s| s.parse().unwrap()).collect();
-    let (x, y) = part1(input);
+    let input: Vec<i32>= lines.iter().map(|s| s.parse().unwrap()).collect();
+    let (x, y) = part1(input.clone());
     println!("answer day 1 is {}", x * y);
+    let (x, y, z) = part2(input.clone());
+    println!("answer day 2 is {}", x * y * z);
     println!("Hello, world!");
 }
