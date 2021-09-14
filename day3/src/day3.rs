840f6ae9ff71db3a37279ec42223b1d9b0c064cd
Anne Marije van der Meer <anne.marije.meer@gmail.com>
2021-09-14 16:47:16 +0200

    day 3 part 2

--- day3/src/day3.rs
+++ day3/src/day3.rs
@@ -2,19 +2,37 @@ use regex::Regex;
 use grid::Grid;
 use crate::utils::grid::CharGrid;
 use crate::utils::grid::Square;
+use std::borrow::Borrow;
 
-pub(crate) fn part1(grid: Grid<char>) -> i32 {
-    let step_x = 3;
-    let step_y = 1;
+pub(crate) fn part1(grid: &Grid<char>) -> i32 {
+   return calculate_trees(grid.borrow(), 3, 1);
+}
+
+pub(crate) fn part2(grid: &Grid<char>) -> i64 {
+    let results = vec![
+        calculate_trees(grid.borrow(), 1, 1),
+        calculate_trees(grid.borrow(), 3, 1),
+        calculate_trees(grid.borrow(), 5, 1),
+        calculate_trees(grid.borrow(), 7, 1),
+        calculate_trees(grid.borrow(), 1, 2),
+    ];
+    let mut outcome:i64 =1;
+    for r in results {
+        outcome *= r as i64;
+    }
+    return outcome;
+}
+
+fn calculate_trees(grid: &Grid<char>, step_x:usize, step_y:usize) -> i32 {
     let mut x: usize = step_x;
     let mut y: usize = step_y;
     let mut tree_count = 0;
-   // for y in 0..grid.rows() {
-   //     for x in 0..2*grid.cols(){
-   //         print!("{}",grid.get_infinite_right(y,x).unwrap());
-   //     }
-   //     print!("\n");
-   // }
+    // for y in 0..grid.rows() {
+    //     for x in 0..2*grid.cols(){
+    //         print!("{}",grid.get_infinite_right(y,x).unwrap());
+    //     }
+    //     print!("\n");
+    // }
     while y < grid.size().0{
         let square_opt = grid.get_infinite_right(x,y);
         if let Some(square)  = square_opt {
@@ -22,8 +40,8 @@ pub(crate) fn part1(grid: Grid<char>) -> i32 {
                 tree_count +=1;
             }
         }
-        x +=3;
-        y +=1;
+        x +=step_x;
+        y +=step_y;
     }
     return tree_count;
 }
@@ -42,7 +60,17 @@ mod tests {
         let lines = read_lines(input_text);
         let grid_input: Vec<Vec<char>> = Vec::from_iter(lines.iter().map( |&s| Vec::from_iter(s.chars()) ));
         let grid = Grid::from_char_vec(grid_input);
-        let part1result = part1(grid);
+        let part1result = part1(grid.borrow());
         assert_eq!(part1result, 7);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input_text = include_str!("../input/day3_test_input.txt");
+        let lines = read_lines(input_text);
+        let grid_input: Vec<Vec<char>> = Vec::from_iter(lines.iter().map( |&s| Vec::from_iter(s.chars()) ));
+        let grid = Grid::from_char_vec(grid_input);
+        let part1result = part2(grid.borrow());
+        assert_eq!(part1result, 336);
     }
 }
--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,9 +4,10 @@ use std::iter::FromIterator;
 
 use grid::Grid;
 
-use crate::day3::part1;
+use crate::day3::{part1, part2};
 use crate::utils::read_lines;
 use crate::utils::grid::CharGrid;
+use std::borrow::Borrow;
 
 
 mod day3;
@@ -16,6 +17,8 @@ fn main() {
     let lines = read_lines(input_text);
     let grid_input: Vec<Vec<char>> = Vec::from_iter(lines.iter().map(|&s| Vec::from_iter(s.chars())));
     let grid = Grid::from_char_vec(grid_input);
-    let part1Result = part1(grid);
+    let part1Result = part1(grid.borrow());
     println!("Answer to day3 part 1 is {}", part1Result);
+    let part2Result = part2(grid.borrow());
+    println!("Answer to day3 part 2 is {}", part2Result);
 }
