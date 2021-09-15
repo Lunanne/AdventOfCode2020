ed1c7de8b84c313af2f6e972cf1870d04945c0fc
Anne Marije van der Meer <anne.marije.meer@gmail.com>
2021-09-15 13:56:59 +0200

    day 6 typo

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let input_text = include_str!("../input/day6_input.txt");
     let input = Vec::from_iter(input_text.split("\n\n"));
     let part1result = part1(Vec::from(input.borrow()));
-    println!("Answer day 4 part 1 {}", part1result);
+    println!("Answer day 6 part 1 {}", part1result);
     let part2result = part2(Vec::from(input.borrow()));
-    println!("Answer day 4 part 2 {}", part2result);
+    println!("Answer day 6 part 2 {}", part2result);
 }
